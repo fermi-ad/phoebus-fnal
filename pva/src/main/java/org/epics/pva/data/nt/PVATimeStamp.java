e2f1978ccefc98def29e655d0307d0332746c62e
Charlie King <kingc@fnal.gov>
2025-05-15 11:56:21 -0500

    added pva module

---
+++ pva/src/main/java/org/epics/pva/data/nt/PVATimeStamp.java
@@ -0,0 +1,143 @@
+/*******************************************************************************
+ * Copyright (c) 2019-2020 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.epics.pva.data.nt;
+
+import org.epics.pva.data.PVAInt;
+import org.epics.pva.data.PVALong;
+import org.epics.pva.data.PVAStructure;
+
+import java.time.Instant;
+
+/**
+ * Normative timestamp type
+ * 
+ * structure
+ *   long secondsPastEpoch
+ *   int nanoseconds
+ *   int userTag
+ * 
+ * @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class PVATimeStamp extends PVAStructure
+{
+    public static final Instant NO_TIME = Instant.ofEpochSecond(0, 0);
+    public static final String TIMESTAMP_NAME_STRING = "timeStamp";
+    public static final String SECONDS_PAST_EPOCH = "secondsPastEpoch";
+    public static final String NANOSECONDS = "nanoseconds";
+    /** Type name for time stamp */
+    public static final String TIME_T = "time_t";
+
+    private final PVALong secs;
+    private final PVAInt nano;
+
+    /** 'now' */
+    public PVATimeStamp()
+    {
+        this(Instant.now());
+    }
+
+    /** @param time Instant */
+    public PVATimeStamp(final Instant time)
+    {
+        this(TIMESTAMP_NAME_STRING, time);
+    }
+
+    /** @param name Name for 'now' */
+    public PVATimeStamp(final String name)
+    {
+        this(name, Instant.now());
+    }
+
+    /** @param name Name
+     *  @param time Time
+     */
+    public PVATimeStamp(final String name, final Instant time)
+    {
+        this(name,
+             new PVALong(SECONDS_PAST_EPOCH, false, time.getEpochSecond()),
+             new PVAInt(NANOSECONDS, false, time.getNano()));
+    }
+
+    /** 
+     * Constructor with PVAData
+     * @param secs secondsPastEpoch
+     *  @param nanos nanoseconds
+     */
+    public PVATimeStamp(final String name, final PVALong secs, final PVAInt nanos)
+    {
+        super(name, TIME_T,
+              secs,
+              nanos,
+              new PVAInt("userTag", 0));
+        this.secs = secs;
+        this.nano = nanos;
+    }
+
+    /** @param time Desired time (seconds, nanoseconds) */
+    public void set(final Instant time)
+    {
+        secs.set(time.getEpochSecond());
+        nano.set(time.getNano());
+    }
+
+    /** Update "timeStamp" in value
+     *  @param value Value
+     *  @param time Desired time (seconds, nanoseconds)
+     */
+    public static void set(final PVAStructure value, final Instant time)
+    {
+        final PVAStructure ts = value.get(TIMESTAMP_NAME_STRING);
+        if (ts == null)
+            throw new IllegalArgumentException("Cannot locate timeStamp in " + value);
+        // Assume a structure "timeStamp" starts with seconds, nano
+        PVALong secs = ts.get(1);
+        PVAInt nano = ts.get(2);
+        secs.set(time.getEpochSecond());
+        nano.set(time.getNano());
+    }
+
+    public Instant instant() {
+        if (secs == null || nano == null)
+                return NO_TIME;
+        else
+            return Instant.ofEpochSecond(secs.get(), nano.get());
+    }
+
+    /** 
+     * Conversion from structure to PVATimeStamp
+     * 
+     * @param structure Potential "time_t" structure
+     *  @return PVATimeStamp or <code>null</code>
+     */
+    public static PVATimeStamp fromStructure(PVAStructure structure) {
+        if (structure != null && structure.getName().equals(TIMESTAMP_NAME_STRING))
+        {
+            final PVALong secs = structure.get(SECONDS_PAST_EPOCH);
+            final PVAInt nano = structure.get(NANOSECONDS);
+            if (secs != null && nano != null) {
+                return new PVATimeStamp(TIMESTAMP_NAME_STRING, secs, nano);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get TimeStamp from a PVAStructure
+     * 
+     * @param structure Structure containing TimeStamp
+     * @return PVATimeStamp or <code>null</code>
+     */
+    public static PVATimeStamp getTimeStamp(PVAStructure structure) {
+        PVAStructure timestampStructure = structure.get(TIMESTAMP_NAME_STRING);
+        if (timestampStructure != null) {
+            return fromStructure(timestampStructure);
+        }
+        return null;
+    }
+}
---
+++ pva/src/main/java/org/epics/pva/server/ServerTCPHandler.java
@@ -0,0 +1,176 @@
+/*******************************************************************************
+ * Copyright (c) 2019-2023 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.epics.pva.server;
+
+import org.epics.pva.common.*;
+import org.epics.pva.common.SecureSockets.TLSHandshakeInfo;
+import org.epics.pva.data.PVASize;
+import org.epics.pva.data.PVAString;
+import org.epics.pva.data.PVATypeRegistry;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Objects;
+import java.util.logging.Level;
+
+import static org.epics.pva.PVASettings.logger;
+
+/** Handler for one TCP-connected client
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+class ServerTCPHandler extends TCPHandler
+{
+    /** Handlers for various commands, re-used whenever a command is received */
+    private static final CommandHandlers<ServerTCPHandler> handlers =
+        new CommandHandlers<>(new ValidationHandler(),
+                              new SearchCommandHandler(),
+                              new EchoHandler(),
+                              new CreateChannelHandler(),
+                              new GetHandler(),
+                              new PutHandler(),
+                              new MonitorHandler(),
+                              new DestroyChannelHandler(),
+                              new GetTypeHandler(),
+                              new DestroyRequestHandler(),
+                              new RPCHandler(),
+                              new CancelHandler());
+
+    /** Server that holds all the PVs */
+    private final PVAServer server;
+
+    /** Info from TLS socket handshake or <code>null</code> */
+    private final TLSHandshakeInfo tls_info;
+
+    /** Types declared by client at other end of this TCP connection */
+    private final PVATypeRegistry client_types = new PVATypeRegistry();
+
+    /** Auth info, e.g. client user info and his/her permissions */
+    private volatile ServerAuth auth = ServerAuth.Anonymous;
+
+
+    public ServerTCPHandler(final PVAServer server, final Socket client, final TLSHandshakeInfo tls_info) throws Exception
+    {
+        super(client, false);
+        this.server = Objects.requireNonNull(server);
+        this.tls_info = tls_info;
+
+        server.register(this);
+        startReceiver();
+        startSender();
+
+        // Initialize TCP connection by setting byte order..
+        submit((version, buffer) ->
+        {
+            logger.log(Level.FINE, () -> "Set byte order " + buffer.order());
+            // Payload size is used as byte order hint
+            // 0xFFFFFFFF: Client needs to check each message for byte order
+            // 0x00000000: Server sends each message in the same byte order,
+            //             but there's still a valid byte order flag,
+            //             so client is free to keep checking each message
+            final int size_used_as_hint = 0x00000000;
+            PVAHeader.encodeMessageHeader(buffer,
+                    (byte) (PVAHeader.FLAG_CONTROL | PVAHeader.FLAG_SERVER),
+                    PVAHeader.CTRL_SET_BYTE_ORDER, size_used_as_hint);
+        });
+        // .. and requesting connection validation
+        submit((version, buffer) ->
+        {
+            logger.log(Level.FINE, () -> "Sending Validation Request");
+
+            final int size_offset = buffer.position() + PVAHeader.HEADER_OFFSET_PAYLOAD_SIZE;
+            PVAHeader.encodeMessageHeader(buffer, PVAHeader.FLAG_SERVER, PVAHeader.CMD_CONNECTION_VALIDATION, 4+2+1);
+            final int payload_start = buffer.position();
+
+            // int serverReceiveBufferSize;
+            buffer.putInt(receive_buffer.capacity());
+
+            // short serverIntrospectionRegistryMaxSize;
+            buffer.putShort(Short.MAX_VALUE);
+
+            // If client identified itself on secure connection, server supports "x509"
+            boolean support_x509 = this.tls_info != null;
+
+            // string[] authNZ; listing most secure at end
+            PVASize.encodeSize(support_x509 ? 3 : 2, buffer);
+            PVAString.encodeString(PVAAuth.ANONYMOUS, buffer);
+            PVAString.encodeString(PVAAuth.CA, buffer);
+            if (support_x509)
+                PVAString.encodeString(PVAAuth.X509, buffer);
+
+            buffer.putInt(size_offset, buffer.position() - payload_start);
+        });
+    }
+
+    PVAServer getServer()
+    {
+        return server;
+    }
+
+    TLSHandshakeInfo getTLSHandshakeInfo()
+    {
+        return tls_info;
+    }
+
+    PVATypeRegistry getClientTypes()
+    {
+        return client_types;
+    }
+
+    void setAuth(final ServerAuth auth)
+    {
+        this.auth = auth;
+    }
+
+    // XXX At this time, nothing uses the auth info
+    ServerAuth getAuth()
+    {
+        return auth;
+    }
+
+    @Override
+    protected void onReceiverExited(final boolean running)
+    {
+        if (running)
+            server.shutdownConnection(this);
+    }
+
+    @Override
+    protected void handleControlMessage(final byte command, final ByteBuffer buffer) throws Exception
+    {
+        if (command == PVAHeader.CTRL_SET_BYTE_ORDER)
+            logger.log(Level.WARNING, "Client sent SET_BYTE_ORDER command?");
+        else
+            super.handleControlMessage(command, buffer);
+    }
+
+    @Override
+    protected void handleApplicationMessage(final byte command, final ByteBuffer buffer) throws Exception
+    {
+        if (!handlers.handleCommand(command, this, buffer))
+            super.handleApplicationMessage(command, buffer);
+    }
+
+    /** Send a "channel found" reply to a client's search
+     *  @param guid This server's GUID
+     *  @param seq Client search request sequence number
+     *  @param cid Client's channel ID or -1
+     *  @param server_address TCP address where client can connect to server
+     *  @param tls Should client use tls?
+     */
+    void submitSearchReply(final Guid guid, final int seq, final int cid, final InetSocketAddress server_address, final boolean tls)
+    {
+        final RequestEncoder encoder = (version, buffer) ->
+        {
+            logger.log(Level.FINER, () -> "Sending " + (tls ? "TLS" : "TCP") + " search reply");
+            SearchResponse.encode(guid, seq, cid, server_address.getAddress(), server_address.getPort(), tls, buffer);
+        };
+        submit(encoder);
+    }
+}
