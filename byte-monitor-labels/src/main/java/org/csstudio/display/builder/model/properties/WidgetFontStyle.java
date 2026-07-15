81f84cf881f83bf52d032d3068e4c00609c99344
Zongwei Yuan x6878 13046N <zyuan@fermi.win.fnal.gov>
2026-07-15 14:16:24 -0500

    Add underline font style support (WidgetFontStyle, JFXUtil, LabelRepresentation, TextUpdateRepresentation, Messages)

---
+++ byte-monitor-labels/src/main/java/org/csstudio/display/builder/model/properties/WidgetFontStyle.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.csstudio.display.builder.model.properties;
+
+import org.csstudio.display.builder.model.Messages;
+
+/** Description of a font style
+ *  @author Kay Kasemir
+ */
+public enum WidgetFontStyle
+{
+    // Ordinals match legacy style bits based on SWT.BOLD, SWT.ITALIC
+    /** Regular */
+    REGULAR(Messages.FontStyle_Regular),
+    /** Bold */
+    BOLD(Messages.FontStyle_Bold),
+    /** Italic */
+    ITALIC(Messages.FontStyle_Italic),
+    /** Bold and italic */
+    BOLD_ITALIC(Messages.FontStyle_BoldItalic),
+    /** Underline */
+    UNDERLINE(Messages.FontStyle_Underline),
+    /** Bold and underline */
+    BOLD_UNDERLINE(Messages.FontStyle_BoldUnderline),
+    /** Italic and underline */
+    ITALIC_UNDERLINE(Messages.FontStyle_ItalicUnderline),
+    /** Bold, italic, and underline */
+    BOLD_ITALIC_UNDERLINE(Messages.FontStyle_BoldItalicUnderline);
+
+    private final String name;
+
+    private WidgetFontStyle(final String name)
+    {
+        this.name = name;
+    }
+
+    /** @return Whether this style includes underline */
+    public boolean isUnderline()
+    {
+        return this == UNDERLINE || this == BOLD_UNDERLINE ||
+               this == ITALIC_UNDERLINE || this == BOLD_ITALIC_UNDERLINE;
+    }
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
+}
